03525133b14c0038d7ade41db831bc9dbfeaaa0e
Carlos Chandia <cgchrnani@gmail.com>
2023-06-30 21:32:18 -0400

    base de datos definida

--- todolist/settings.py
+++ todolist/settings.py
@@ -76,13 +76,24 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+   # DATABASES = {
+   #     'default': {
+   #         'ENGINE': 'django.db.backends.sqlite3',
+   #         'NAME': BASE_DIR / 'db.sqlite3',
+   #     }
+   # }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cchandia_todolist',
+        'USER' : 'cchandia_test',
+        'PASSWORD': "%123456%",
+        'HOST':'postgresql-cchandia.alwaysdata.net',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
